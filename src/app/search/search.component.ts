dcd243aeb0e566f0e16e0caf53841709366d2c17
umavaragam <umavaragam.ps@gmail.com>
2022-01-17 01:01:38 +0530

    home page ui changes

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-search',
@@ -6,10 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search.component.scss']
 })
 export class SearchComponent implements OnInit {
+  showMaps: boolean = true;
 
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  userDetailsForm = new FormGroup({
+    bussinessName: new FormControl(''),
+    primaryContactName: new FormControl(''),
+    primaryContactNumber: new FormControl(''),
+    secondaryContactName: new FormControl(''),
+    secondaryContactNumber: new FormControl(''),
+    secondaryEmailId: new FormControl(''),
+    pANNo: new FormControl(''),
+    bankName: new FormControl(''),
+    primaryEmailid: new FormControl(''),
+    accountNumber: new FormControl(''),
+    ifscCode: new FormControl(''),
+    gstNo: new FormControl(''),
+  });
 }
